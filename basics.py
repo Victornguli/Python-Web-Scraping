7d6b9889d6f4a56f2b2706c2cfbb3fab244b382d
Victor <victornjoseph@gmail.com>
2019-05-23 17:39:51 +0300

    modified basics.py

--- basics.py
+++ basics.py
@@ -1,4 +1,12 @@
-import urllib2
+import requests
 from bs4 import BeautifulSoup
 
-quote_page = ‘http://www.bloomberg.com/quote/SPX:IND'
+quote_page = "https://www.cdnperf.com/"
+page = requests.get(quote_page)
+soup = BeautifulSoup(page.text, "html.parser")
+
+#name_box = soup.find("ul", attrs={"class" :"dropdown-menu"}).find("li")
+#name = name_box
+#print (name_box)
+
+print(soup.find_all("ul"))
